8ceca92c498c1654539a8623dc3d61f1b82b8a24
nitinahlawat12 <130631535+nitinahlawat12@users.noreply.github.com>
2024-09-17 19:58:24 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "98791d88e5da2b6cc3309b48998ed67c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
